ee005c538ca319bb5c6589a7e37978cf85a45481
thanakritks <Thanakrit.k@chareontut.co.th>
2025-01-28 11:51:48 +0700

    fix main.gol

--- main.go
+++ main.go
@@ -1,96 +1,96 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "os"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 // Define the output functions
 func GenerateTXT(sbom SBOM, outputFile string) error {
-    // Implementation for generating TXT output
-    return nil
+	// Implementation for generating TXT output
+	return nil
 }
 
 func GenerateCSV(sbom SBOM, outputFile string) error {
-    // Implementation for generating CSV output
-    return nil
+	// Implementation for generating CSV output
+	return nil
 }
 
 func GenerateXML(sbom SBOM, outputFile string) error {
-    // Implementation for generating XML output
-    return nil
+	// Implementation for generating XML output
+	return nil
 }
 
 func GeneratePDF(sbom SBOM, outputFile string) error {
-    // Implementation for generating PDF output
-    return nil
+	// Implementation for generating PDF output
+	return nil
 }
 
 // SBOM represents the structure of the SBOM JSON
 type SBOM struct {
-    Components []Component `json:"components"`
+	Components []Component `json:"components"`
 }
 
 // Component represents a single component in the SBOM
 type Component struct {
-    Name    string `json:"name"`
-    Version string `json:"version"`
-    License string `json:"license"`
+	Name    string `json:"name"`
+	Version string `json:"version"`
+	License string `json:"license"`
 }
 
 func main() {
-    // Check if the user provided an SBOM file and output format
-    if len(os.Args) < 3 {
-        fmt.Println("Usage: sbom2doc <sbom-file> <output-format> [output-file]")
-        fmt.Println("Supported formats: txt, csv, xml, pdf")
-        os.Exit(1)
-    }
+	// Check if the user provided an SBOM file and output format
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: sbom2doc <sbom-file> <output-format> [output-file]")
+		fmt.Println("Supported formats: txt, csv, xml, pdf")
+		os.Exit(1)
+	}
 
-    sbomFile := os.Args[1]
-    outputFormat := os.Args[2]
-    outputFile := "output." + outputFormat // Default output file name
+	sbomFile := os.Args[1]
+	outputFormat := os.Args[2]
+	outputFile := "output." + outputFormat // Default output file name
 
-    // Allow custom output file name
-    if len(os.Args) > 3 {
-        outputFile = os.Args[3]
-    }
+	// Allow custom output file name
+	if len(os.Args) > 3 {
+		outputFile = os.Args[3]
+	}
 
-    // Read the SBOM file
-    data, err := ioutil.ReadFile(sbomFile)
+	// Read the SBOM file
+	data, err := ioutil.ReadFile(sbomFile)
 	if err != nil {
 		fmt.Printf("Error reading SBOM file: %s\n", err)
-        os.Exit(1)
-    }
+		os.Exit(1)
+	}
 
-    // Parse the SBOM JSON
-    var sbom SBOM
-    err = json.Unmarshal(data, &sbom)
-    if err != nil {
-        fmt.Printf("Error parsing SBOM JSON: %s\n", err)
-        os.Exit(1)
-    }
+	// Parse the SBOM JSON
+	var sbom SBOM
+	err = json.Unmarshal(data, &sbom)
+	if err != nil {
+		fmt.Printf("Error parsing SBOM JSON: %s\n", err)
+		os.Exit(1)
+	}
 
-    // Generate the output based on the chosen format
-    switch outputFormat {
-    case "txt":
-        err = GenerateTXT(sbom, outputFile)
-    case "csv":
-        err = GenerateCSV(sbom, outputFile)
-    case "xml":
-        err = GenerateXML(sbom, outputFile)
-    case "pdf":
-        err = GeneratePDF(sbom, outputFile)
-    default:
-        fmt.Printf("Unsupported output format: %s\n", outputFormat)
-        os.Exit(1)
-    }
+	// Generate the output based on the chosen format
+	switch outputFormat {
+	case "txt":
+		err = GenerateTXT(sbom, outputFile)
+	case "csv":
+		err = GenerateCSV(sbom, outputFile)
+	case "xml":
+		err = GenerateXML(sbom, outputFile)
+	case "pdf":
+		err = GeneratePDF(sbom, outputFile)
+	default:
+		fmt.Printf("Unsupported output format: %s\n", outputFormat)
+		os.Exit(1)
+	}
 
-    if err != nil {
-        fmt.Printf("Error generating %s: %s\n", outputFormat, err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Printf("Error generating %s: %s\n", outputFormat, err)
+		os.Exit(1)
+	}
 
-    fmt.Printf("Successfully generated %s\n", outputFile)
+	fmt.Printf("Successfully generated %s\n", outputFile)
 }
