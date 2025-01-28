b800ded758ba76e52e89a2766f2244975013452b
thanakritks <Thanakrit.k@chareontut.co.th>
2025-01-28 13:43:36 +0700

    fix add sbom.go

--- output/txt.go
+++ output/txt.go
@@ -1,24 +1,29 @@
 package output
 
 import (
-    "fmt"
-    "os"
-    "github.com/thanakritks/sbom-2-doc-go/sbom"
+	"fmt"
+	"os"
+
+	"github.com/thanakritks/sbom-2-doc-go/sbom"
 )
 
 func GenerateTXT(sbom sbom.SBOM, outputFile string) error {
-    file, err := os.Create(outputFile)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
+	file, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-    for _, component := range sbom.Components {
-        _, err := fmt.Fprintf(file, "Name: %s, Version: %s, License: %s\n", component.Name, component.Version, component.License)
-        if err != nil {
-            return err
-        }
-    }
+	fmt.Fprintln(file, "╭──────────────╮")
+	fmt.Fprintln(file, "│ SBOM Summary │")
+	fmt.Fprintln(file, "╰──────────────╯")
 
-    return nil
+	for _, component := range sbom.Components {
+		_, err := fmt.Fprintf(file, "Name: %s, Version: %s, License: %s\n", component.Name, component.Version, component.License)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
